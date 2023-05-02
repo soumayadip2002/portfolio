ac43c3ff60e3a23848303375c821595162bf7451
soumayadip2002 <soumayadipsaha2002@gmail.com>
2023-05-02 08:28:17 +0530

    update

--- src/components/project/Project.jsx
+++ src/components/project/Project.jsx
@@ -1,11 +1,126 @@
-import React from 'react'
-import './project.css';
+import React from "react";
+import "./project.css";
+import IMG1 from "../../img/blog.jpg";
+import IMG2 from "../../img/calculator.jpg";
+import IMG3 from "../../img/e-commerce.png";
+import IMG4 from "../../img/Beige minimalist photo collage.png";
+import IMG5 from "../../img/Green natural waterfall photo collage.png";
+import IMG6 from "../../img/RSP Confarence Hub Publication C.jpg";
+import IMG7 from "../../img/divorce_page-0001.jpg";
+import IMG8 from "../../img/port.png";
+import IMG9 from "../../img/spotify.webp";
+import IMG10 from "../../img/tourist.png";
+
+const project_data = [
+  {
+    id: 1,
+    image: IMG1,
+    title: "CMS management system",
+    github: "https://github.com",
+    demo: "https://github.com",
+  },
+
+  {
+    id: 2,
+    image: IMG2,
+    title: "calculator",
+    github: "https://github.com",
+    demo: "https://github.com",
+  },
+
+  {
+    id: 3,
+    image: IMG3,
+    title: "e commerce website",
+    github: "https://github.com",
+    demo: "https://github.com",
+  },
+
+  {
+    id: 4,
+    image: IMG4,
+    title: "food shop",
+    github: "https://github.com",
+    demo: "https://github.com",
+  },
+
+  {
+    id: 5,
+    image: IMG5,
+    title: "Grocery Shop",
+    github: "https://github.com",
+    demo: "https://github.com",
+  },
+
+  {
+    id: 6,
+    image: IMG6,
+    title: "crime rate prediction",
+    github: "https://github.com",
+    demo: "https://github.com",
+  },
+
+  {
+    id: 7,
+    image: IMG7,
+    title: "divorce rate prediction",
+    github: "https://github.com",
+    demo: "https://github.com",
+  },
+
+  {
+    id: 8,
+    image: IMG8,
+    title: "portfolio",
+    github: "https://github.com",
+    demo: "https://github.com",
+  },
+
+  {
+    id: 9,
+    image: IMG9,
+    title: "Music website",
+    github: "https://github.com",
+    demo: "https://github.com",
+  },
+
+  {
+    id: 10,
+    image: IMG10,
+    title: "Tourist guide",
+    github: "https://github.com",
+    demo: "https://github.com",
+  },
+];
+
 const Project = () => {
   return (
     <section id="project">
-      project
-    </section>
-  )
-}
+      <h5>my recent work</h5>
+      <h2>projects</h2>
 
-export default Project
+      <div className="container project_container">
+        {project_data.map(({ id, image, title, github, demo }) => {
+          return (
+            <article key={id} className="portfolio_item">
+              <div className="portfolio_item-img">
+                <img src={image} alt={title} />
+              </div>
+              <h3>{title}</h3>
+              <div className="portfolio_item-cta">
+                <a href={github} className="btn" target="_blank">
+                  github
+                </a>
+                <a href={demo} className="btn btn-primary" target="_blank">
+                  live demo
+                </a>
+              </div>
+            </article>
+          );
+        })}
+      </div>
+    </section>
+  );
+};
+
+export default Project;
--- src/components/testimonals/Testimonal.jsx
+++ src/components/testimonals/Testimonal.jsx
@@ -1,11 +1,62 @@
-import React from 'react'
-import './testimonal.css'
+import React from "react";
+import "./testimonal.css";
+import PIC1 from "../../img/pic-1.png";
+import PIC2 from "../../img/pic-2.png";
+import PIC3 from "../../img/pic-3.png";
+import PIC4 from "../../img/pic-4.png";
+const client = [
+  {
+    id: 1,
+    image: PIC1,
+    name: "jhone deo",
+    review:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. At repudiandae iure maiores, nesciunt recusandae illo?",
+  },
+  {
+    id: 2,
+    image: PIC2,
+    name: "akash saha",
+    review:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. At repudiandae iure maiores, nesciunt recusandae illo?",
+  },
+  {
+    id: 3,
+    image: PIC3,
+    name: "soumya saha",
+    review:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. At repudiandae iure maiores, nesciunt recusandae illo?",
+  },
+  {
+    id: 4,
+    image: PIC4,
+    name: "wally west",
+    review:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. At repudiandae iure maiores, nesciunt recusandae illo?",
+  },
+];
 const Testimonal = () => {
   return (
     <section id="testimonals">
-      testimonals
-    </section>
-  )
-}
+      <h5>review from clients</h5>
+      <h2>testimonals</h2>
 
-export default Testimonal
+      <div className="container testimonal_container">
+        {client.map(({ id, image, name, review }) => {
+          return (
+            <article key={id} className="testimonal">
+              <div className="client_avatar">
+                <img src={image} alt="" />
+              </div>
+
+              <h5>{name}</h5>
+
+              <small className="client_review">{review}</small>
+            </article>
+          );
+        })}
+      </div>
+    </section>
+  );
+};
+
+export default Testimonal;
